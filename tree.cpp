1eefe5902f2dfa31431f1c9fb88d76cd34e022ba
kuldeep yadav <kulcodes@gmail.com>
2026-09-24 22:02:20 +0530

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -7,3 +7,12 @@ int height(Node* root) {
 
     return 1 + max(leftHeight, rightHeight);
 }
+
+
+int countNodes(Node* root) {
+    if (root == NULL)
+        return 0;
+
+    return 1 + countNodes(root->left)
+             + countNodes(root->right);
+}
