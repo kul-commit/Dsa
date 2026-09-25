e1fb0aedf075b49ba5c933af86be2e859fd5d8d8
kuldeep yadav <kulcodes@gmail.com>
2026-09-25 21:49:56 +0530

    Sorting.cpp

---
+++ Sorting.cpp
@@ -0,0 +1,46 @@
+//Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+Time: O(n²)
+//Selection Sort
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+Time: O(n²)
+
+  
+//  Insertion Sort
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+Time: O(n²)
+
