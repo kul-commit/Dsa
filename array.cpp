509830270a7fd7773c8da1cbb7f637434f75fa59
kuldeep yadav <kulcodes@gmail.com>
2026-09-25 21:48:06 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -38,3 +38,33 @@ int main() {
 
     return 0;
 }
+
+
+
+//1. Array
+//Traversal
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+Linear Search
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+Time: O(n)
+
